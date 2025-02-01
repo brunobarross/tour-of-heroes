e77e0011b333cdae9b7a8226e241e6023b799160
Altamiro Bruno <altamiro_bruno@yahoo.com.br>
2025-02-01 15:16:50 -0300

    adicionando services e messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,7 @@ import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,13 +13,16 @@ import { HeroService } from '../services/hero.service';
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
-
 export class HeroesComponent {
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
   heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelectedHero(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   getHeroes() {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
